cb2f25c810e7a2fdd48356f7cf9f90ff16ad0f75
elenbeilina <elenbeilina>
2020-11-03 19:13:18 +0300

    fix

--- src/utils/Converter.java
+++ src/utils/Converter.java
@@ -15,20 +15,6 @@ public class Converter {
         }
     }
 
-    void conToBinary(String s) {
-        byte[] arr = txtToByte(s);
-        StringBuilder binary = new StringBuilder();
-        for (byte b : arr) {
-            int val = b;
-            for (int i = 0; i < 8; i++) {
-                binary.append((val & 128) == 0 ? 0 : 1);
-                val <<= 1;
-            }
-        }
-        System.out.println("'" + s + "' to binary: " + binary);
-        conToString(binary);
-    }
-
     public static byte[] txtToByte(String s) {
         return s.getBytes(StandardCharsets.UTF_8);
     }
